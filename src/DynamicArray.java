db63d9d115b65b6f4a08551a9ca1ec2e1323ea87
Alexandr456731 <aleksandrburnaev40@gmail.com>
2024-11-15 15:07:57 +0300

    Update DynamicArray.java

--- src/DynamicArray.java
+++ src/DynamicArray.java
@@ -1,46 +1,46 @@
-public static class DynamicArray<T> {
+public static class DynamicArray<T>  {
 
     private T[] array;
-    public int length;
+    private int length;
+    private int V;
 
-    public  DynamicArray(){this.array = (T[]) new Object[]{};}
-    public DynamicArray(T[] array){
-        setArray(array);
-    }
 
-    private void setArray(T[] array){
-        this.array = array;
+    public DynamicArray(){
+        this.length = 0;
+        this.V = 10;
+        this.array = (T[]) new Object[this.V];
     }
 
     public void add(T el){
-        T[] oldArray = this.array.clone();
-        Object[] array = new Object[oldArray.length + 1];
-
-        for(byte i = 0; i < oldArray.length; i++){
-            array[i] = oldArray[i];
+        if(this.V - this.length < 1){
+            this.V += 5;
+            T[] newArray = (T[]) new Object[this.V];
+            System.arraycopy(this.array, 0, newArray, 0, this.length);
+            this.array = newArray;
         }
-        array[array.length - 1] = el;
-
-        this.length = array.length;
-        this.array = (T[]) array;
+        this.array[this.length] = el;
+        this.length++;
     }
-    public void remove(int index){
-        T[] oldArray = this.array.clone();
-        Object[] array = new Object[oldArray.length - 1];
-
-        for(byte i = 0; i < oldArray.length; i++){
-            if(i == index){
-                continue;
-            }else if(i < index) {
-                array[i] = oldArray[i];
-            }else{
-                array[i - 1] = oldArray[i];
-            }
+    public void remove(int index) {
+        if(this.V - this.length > 4) {
+            this.V -= 5;
         }
 
-        this.length = array.length;
-        this.array = (T[]) array;
+        T[] newArray = (T[]) new Object[this.V];
+        System.arraycopy(this.array, 0 , newArray, 0, index);
+        System.arraycopy(this.array, index + 1, newArray, index, this.length - index - 1);
+
+        this.array = newArray;
+        this.length--;
     }
+    public int getLength(){
+        return this.length;
+    }
+
+    public int getV() {
+        return this.V;
+    }
+
     public T get(int index){
         return this.array[index];
     }
