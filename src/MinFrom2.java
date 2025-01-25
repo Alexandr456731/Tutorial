9d38c3da9f9560e347d5710d60920c9aab658bb5
Alexandr456731 <aleksandrburnaev40@gmail.com>
2025-01-25 20:07:35 +0300

    Create MinFrom2.java

---
+++ src/MinFrom2.java
@@ -0,0 +1,11 @@
+public class MinFrom2 {
+    public static int minFrom2 (int a, int b){
+        if(a > b){
+            return a;
+        }else if(a < b){
+            return b;
+        } else{
+            return 0;
+        }
+    }
+}
