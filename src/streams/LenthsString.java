2cc9aec7f351e6defad805e20ce28a1053aa3ce0
Alexandr456731 <aleksandrburnaev40@gmail.com>
2025-02-12 15:52:29 +0300

    Update LenthsString.java

--- src/streams/LenthsString.java
+++ src/streams/LenthsString.java
@@ -4,11 +4,6 @@ public class LenthsString{
     public static void main (String[] args) {
         List<String> words = List.of("Java", "Stream", "API", "Lambda");
 
-        List<Integer> lengths = new ArrayList<>();
-        for(String el : words){
-            lengths.add(el.length());
-        }
-
-        lengths.forEach(el -> System.out.print(el + " "));
+        words.stream().mapToInt(el -> el.length()).forEach(System.out::println);
     }
 }
