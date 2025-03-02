c0046acddda8b06d15f638b7ec8dfe0d4146b03a
Alexandr456731 <aleksandrburnaev40@gmail.com>
2025-03-02 19:42:36 +0300

    Update ConsoleCalculator.java

--- src/ConsoleCalculator/ConsoleCalculator.java
+++ src/ConsoleCalculator/ConsoleCalculator.java
@@ -2,75 +2,74 @@ import java.util.*;
 
 class ConsoleCalculator{
         private static Scanner scan = new Scanner(System.in);
-        public static double getInt(){
-            int number;
-            while (true){
-                try{
-                    System.out.print("Введите число: ");
-                    number = scan.nextInt();
-                    return number;
-                }catch (InputMismatchException e){
-                    System.out.println("Ошибка!");
-                    scan.nextLine();
-                }
-            }
-        }
-
-        public static Type getOperation(){
-            String stroka = "e";
-
-            boolean work = true;
-            System.out.println("\"+\" - сложение");
-            System.out.println("\"-\" - вычитаение");
-            System.out.println("\"*\" - умножение");
-            System.out.println("\"/\" - деление");
-            while (work){
-                System.out.print("Введите тип действия: ");
+    public static double getDouble(){
+        double number;
+        while (true){
+            try{
+                System.out.print("Введите число: ");
+                number = Double.parseDouble(scan.nextLine());
+                return number;
+            }catch (InputMismatchException e){
+                System.out.println("Ошибка!");
                 scan.nextLine();
-                stroka = scan.nextLine();
-
-                switch (stroka){
-                    case "+", "-", "*", "/":
-                        work = false;
-                        break;
-                    default:
-                        System.out.println("ошибка");
-                }
             }
+        }
+    }
+
+    public static Type getOperation(){
+        String stroka = "e";
+
+        boolean work = true;
+        System.out.println("\"+\" - сложение");
+        System.out.println("\"-\" - вычитаение");
+        System.out.println("\"*\" - умножение");
+        System.out.println("\"/\" - деление");
+        while (work){
+            System.out.print("Введите тип действия: ");
+            stroka = scan.nextLine();
+
             switch (stroka){
-                case "+":
-                    return Type.ADDITION;
-                case "-":
-                    return Type.SUBTRACTION;
-                case "*":
-                    return Type.MULTIPLICATION;
-                case "/":
-                    return Type.DIVISION;
+                case "+", "-", "*", "/":
+                    work = false;
+                    break;
                 default:
-                    throw new IllegalArgumentException("Вы ввели не то!");
+                    System.out.println("ошибка");
             }
         }
-
-        public static double calc(double num1, double num2, Type operation){
-            switch (operation){
-                case Type.ADDITION:
-                    return num1 + num2;
-                case Type.SUBTRACTION:
-                    return num1 - num2;
-                case Type.MULTIPLICATION:
-                    return num1 * num2;
-                case Type.DIVISION:
-                    return num1 / num2;               
-                default:
-                    throw new IllegalArgumentException("Критическая ошибка");
-            }
+        switch (stroka){
+            case "+":
+                return Type.ADDITION;
+            case "-":
+                return Type.SUBTRACTION;
+            case "*":
+                return Type.MULTIPLICATION;
+            case "/":
+                return Type.DIVISION;
+            default:
+                throw new IllegalArgumentException("Вы ввели не то!");
         }
+    }
 
-        public static void main(String[] args){
-            System.out.println("Резкльтат: " + calc(getInt(), getInt(), getOperation()));
+    public static double calc(double num1, double num2, Type operation){
+        switch (operation){
+            case Type.ADDITION:
+                return num1 + num2;
+            case Type.SUBTRACTION:
+                return num1 - num2;
+            case Type.MULTIPLICATION:
+                return num1 * num2;
+            case Type.DIVISION:
+                return num1 / num2;
+            default:
+                throw new IllegalArgumentException("Критическая ошибка");
         }
+    }
 
-        private enum Type {
-            ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION}
+    public static void main(String[] args){
+        System.out.println("Резкльтат: " + calc(getDouble(), getDouble(), getOperation()));
+    }
+
+    private enum Type {
+        ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION}
 
     }
